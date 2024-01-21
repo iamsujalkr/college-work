ba3c58ed5997026452a9d9f4030c11f35ecd8cf1
Sujal Kumar <sujalwork09@gmail.com>
2024-01-21 23:03:25 +0530

    New File

---
+++ armstrong.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+#include <math.h> // for pow() 
+
+void main() {
+    int num,tnum;
+    printf("Enter the number :  ");
+    scanf("%d", &num);
+    tnum = num;
+    int count = 0;
+    while (tnum > 0) {
+        tnum /= 10;
+        count++;
+    }
+
+    tnum = num;
+    int rem;
+    double sum = 0;
+    while (tnum > 0) {
+        rem = tnum % 10;
+        tnum /= 10;
+        sum += pow(rem,count);
+    }
+
+    if ((int)sum == num) {
+        printf("%d is an armstrong number", num);
+    } else {
+        printf("%d is not an armstrong number", num);
+    }
+
+}
+
+{
+    
+Enter the number :  153
+153 is an armstrong number
+
+Enter the number :  200
+200 is not an armstrong number
+
+}
