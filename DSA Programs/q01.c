eda31a04ee2d6b793c68de9a52cb5d681ee1ef3e
Sujal Kumar <sujalwork09@gmail.com>
2024-02-28 19:08:25 +0530

    Updated q01

--- DSA Programs/q01.c
+++ DSA Programs/q01.c
@@ -1,13 +1,18 @@
 //Program to perform Insertion, Deletion, Traversing and Reverse operations on an Array
 #include <stdio.h>
+#include <stdlib.h>
 
 //declaring an Array to perform operations
 int arr[100];
 int used_size = 0;
 
 // Declaring funtions to perform operations
-int input_array();
-void sorted_insertion();
+void input_array();
+void array_insert(int add_pos, int element);
+void display_array();
+void array_delete(int element);
+void reverse_array();
+
 
 void main(){
     //displaying available operations to user and taking input
@@ -23,22 +28,54 @@ void main(){
     scanf("%d", &user_opr);
     printf("\n");
 
+    int element;
+    int add_pos;
     //performing the valid operation user wants to perform
     switch (user_opr)
     {
     case 1:
         input_array();
-
+        add_pos = used_size;
+        printf("Enter the element to insert :  ");
+        scanf("%d", &element);
+        for(int i = 0; i < used_size; i++){
+            if(element < arr[i]){
+                add_pos = i;
+            }
+        }
+        array_insert(add_pos, element);
+        printf("Updated Array :- ");
+        display_array();
         break;
     case 2:
+        input_array();
+        printf("Enter the element to insert :  ");
+        scanf("%d", &element);
+        printf("Enter the index :  ");
+        scanf("%d", &add_pos);
+        array_insert(add_pos, element-1);
+        printf("Updated Array :- ");
+        display_array();
         break;
     case 3:
-        break;
     case 4:
+        input_array();
+        printf("Enter the element to delete :  ");
+        scanf("%d", &element);
+        array_delete(element);
+        printf("Updated Array :- ");
+        display_array();
         break;
     case 5:
+        input_array();
+        printf("Traversing the Array :- ");
+        display_array();
         break;
     case 6:
+        input_array();
+        reverse_array();
+        printf("Updated Array :- ");
+        display_array();
         break;
     default:
         printf("Oops! You entered an invalid operation");
@@ -46,7 +83,7 @@ void main(){
     }
 }
 
-int input_array(){
+void input_array(){
     /* Takes elements from user to insert in an empty array and sets the used_size */
 
     int n_elements;
@@ -61,7 +98,68 @@ int input_array(){
             scanf("%d", &arr[i]);
             used_size++;
         }
-        return 1;
     }
-    return 0;
+    else{
+        printf("Error! in inputting array");
+        exit(1);
+    }
+}
+
+void array_insert(int add_pos, int element){
+    /* Inserts the given element at the specified index*/
+
+    // Shifing array elements 
+    for(int i = used_size; i >= add_pos; i--){
+        arr[i] = arr[i-1];
+    }
+    // Inserting element at the specified index and incrementing used_size of array
+    arr[add_pos] = element;
+    used_size++;
+}
+
+void display_array(){
+    /* Displays all the elements of the array */
+
+    if(used_size >= 1){
+        for(int i = 0; i < used_size; i++){
+            printf("\nElement %d :  %d", i+1, arr[i]);
+        }
+    }
+    else{
+        printf("The Array is empty!");
+    }
+}
+
+void array_delete(int element){
+    /* Deletes the given element from the array */
+
+    // Searches for element in the array for its index
+    int del_pos = -1;
+    for(int i = 0; i < used_size; i++){
+        if(arr[i] == element){
+            del_pos = i;
+        }
+    }
+
+    // If element not found exit from program
+    if(del_pos == -1){
+        printf("Cannot delete, Element not found in array");
+        exit(1);
+    }
+
+    // If element found deletes the element
+    for(int i = del_pos; i < used_size-1; i++){
+        arr[i] = arr[i+1];
+    }
+    used_size--;
+} 
+
+void reverse_array(){
+    /* Reverses the array elements */
+
+    for(int i = 0, j = used_size-1; i < used_size/2; i++, j--){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
