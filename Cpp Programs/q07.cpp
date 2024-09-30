b9a9b5fd605f9e9dc0316bc664e2d6ec9a005401
Sujal Kumar <sujalwork09@gmail.com>
2024-09-30 21:12:40 +0530

    Added Achievements and Other Works

--- Cpp Programs/q07.cpp
+++ Cpp Programs/q07.cpp
@@ -8,7 +8,13 @@ int main()
     string str;
     cout << "Enter a string : ";
     getline(cin, str);
-    reverse(str.begin(), str.end());
-    cout << "Reversed string: " << str;
+    string str2 = str;
+    reverse(str2.begin(), str2.end());
+    if(str == str2){
+        cout << "The given string is plalindrome";
+    }
+    else{
+        cout << "The given string is not a palindrome";
+    }
     return 0;
 }
